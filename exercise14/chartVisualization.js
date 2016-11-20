ff14353f670763cb9e6171bd914987878fe1e447
piolAtif <spreeti@thoughtworks.com>
2016-11-20 18:28:37 +0530

    Added method for pie chart

--- exercise14/chartVisualization.js
+++ exercise14/chartVisualization.js
@@ -7,6 +7,7 @@ var svg = d3.select('body').append('svg').attr("width",WIDTH).attr("height",HEIG
 
 var arc = d3.arc().innerRadius(0).outerRadius(150);
 var pie = d3.pie().value(function(d){return d}).sort(null);
+
 var path = svg.selectAll('path')
 	.data(pie(setOfValues)).enter()
 	.append('path')
