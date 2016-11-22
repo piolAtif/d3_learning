1d985b194111bf309f5b2d1e4e7f5dc30975a9cf
piolAtif <spreeti@thoughtworks.com>
2016-11-22 16:48:19 +0530

    Refactored drawValue and extract method from it

--- exercise18/basicArrayStatisticVisualization.js
+++ exercise18/basicArrayStatisticVisualization.js
@@ -1,16 +1,51 @@
 var set = [];
 
+var allDetails = d3.select('body').append('div');
+
+var number = function(data){
+	return allDetails.append('div').text('Number(x):').append('span').attr('class','value').text(data);
+}
+
+var min = function(data){
+	return allDetails.append('div').text('MinOfNumber(min[x]):').append('span').attr('class','value').text(d3.min(data));
+}
+
+var max = function(data){
+	return allDetails.append('div').text('MaxOfNumber(max[x]):').append('span').attr('class','value').text(d3.max(data));
+}
+
+var extent = function(data){
+	return allDetails.append('div').text('Extent of number:').append('span').attr('class','value').text(d3.extent(data));
+}
+
+var sum = function(data){
+	return allDetails.append('div').text('SumOfNumber(∑x):').append('span').attr('class','value').text(d3.sum(data));
+}
+
+var mean = function(data){
+	return allDetails.append('div').text('MeanOfNumber(Ẍ):').append('span').attr('class','value').text(d3.mean(data));
+}
+
+var median = function(data){
+	return allDetails.append('div').text('Median of number(m):').append('span').attr('class','value').text(d3.median(data));
+}
+
+var quantile = function(data){
+	return allDetails.append('div').text('Quantile of 6th number(q6):').append('span').attr('class','value').text(d3.quantile(data,0.6));
+}
+
+var variance = function(data){
+	return allDetails.append('div').text('Variance of number(s):').append('span').attr('class','value').text(d3.variance(data));
+}
+
+var deviation = function(data){
+	return allDetails.append('div').text('Deviation of number(𝞂):').append('span').attr('class','value').text(d3.deviation(data));
+}
+
+var setOfOperation = [number,min,max,extent,sum,mean,median,quantile,variance,deviation];
+
 drawValue =function(data){
-	d3.select('#number').append('span').attr('class','value').text(data);
-	d3.select('#min').append('span').attr('class','value').text(d3.min(data));
-	d3.select('#max').append('span').attr('class','value').text(d3.max(data));
-	d3.select('#extent').append('span').attr('class','value').text(d3.extent(data));
-	d3.select('#sum').append('span').attr('class','value').text(d3.sum(data));
-	d3.select('#mean').append('span').attr('class','value').text(d3.mean(data));
-	d3.select('#median').append('span').attr('class','value').text(d3.median(data));
-	d3.select('#quantile').append('span').attr('class','value').text(d3.quantile(data,0.6));
-	d3.select('#variance').append('span').attr('class','value').text(d3.variance(data));
-	d3.select('#deviation').append('span').attr('class','value').text(d3.deviation(data));
+	return setOfOperation.map((operation)=>operation(data));
 }
 
 var enterData = function(){
@@ -19,7 +54,6 @@ var enterData = function(){
 }
 
 var showDetail = function(){
-	d3.select('.allDetails').style('visibility','visible');
 	drawValue(set);
 }
 
