a0d3e8ae25d81648a3b6e1646c40c24dfbeea7bf
piolAtif <spreeti@thoughtworks.com>
2016-11-22 13:29:29 +0530

    Added input and button for entering data for evaluate

--- exercise18/basicArrayStatisticVisualization.js
+++ exercise18/basicArrayStatisticVisualization.js
@@ -1,22 +1,33 @@
 var array = [1,2,3,4,5,6,7];
 var string = ["st","nd","rd","th","th","th","th"];
+var set = [];
 
-var extent = d3.extent(array, function(array){return array+string[array-1]});
-var max = d3.max(array);
-var min = d3.min(array);
-var sum = d3.sum(array);
+
+
 
 var body = d3.select('body');
-d3.select('#number').append('span').attr('class','value').text(array);
-d3.select('#min').append('span').attr('class','value').text(d3.min(array));
-d3.select('#max').append('span').attr('class','value').text(d3.max(array));
-d3.select('#extent').append('span').attr('class','value').text(extent);
-d3.select('#sum').append('span').attr('class','value').text(d3.sum(array));
-d3.select('#mean').append('span').attr('class','value').text(d3.mean(array));
-d3.select('#median').append('span').attr('class','value').text(d3.median(array));
-d3.select('#quantile').append('span').attr('class','value').text(d3.quantile(array,0.6));
-d3.select('#variance').append('span').attr('class','value').text(d3.variance(array));
-d3.select('#deviation').append('span').attr('class','value').text(d3.deviation(array));
+drawValue =function(data){
+	d3.select('#number').append('span').attr('class','value').text(data);
+	d3.select('#min').append('span').attr('class','value').text(d3.min(data));
+	d3.select('#max').append('span').attr('class','value').text(d3.max(data));
+	d3.select('#extent').append('span').attr('class','value').text(d3.extent(data));
+	d3.select('#sum').append('span').attr('class','value').text(d3.sum(data));
+	d3.select('#mean').append('span').attr('class','value').text(d3.mean(data));
+	d3.select('#median').append('span').attr('class','value').text(d3.median(data));
+	d3.select('#quantile').append('span').attr('class','value').text(d3.quantile(data,0.6));
+	d3.select('#variance').append('span').attr('class','value').text(d3.variance(data));
+	d3.select('#deviation').append('span').attr('class','value').text(d3.deviation(data));
+}
+
+var enterData = function(){
+	var values = (document.getElementById('numberValue').value).split(' ');
+	set = values.map((value)=>+value);
+}
+
+var showDetail = function(){
+	d3.select('.allDetails').style('visibility','visible');
+	drawValue(set);
+}
 
 
 
